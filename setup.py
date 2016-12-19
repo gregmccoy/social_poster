0fcb1caec28870e7a5fbc0bda33f1c3347f9a867
Greg McCoy <gregmccoy@gfa.org>
2016-12-19 14:27:55 -0500

    Folder rename

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="social",
+    name="social_poster",
     version="0.1",
     license="GPL",
     description="A social media poster",
     author="Greg McCoy",
     author_email="gmccoy4242@gmail.com",
-    url="https://github.com/gregmccoy/python-social",
+    url="https://github.com/gregmccoy/social_poster",
     install_requires=[
         'requests',
         'google-api-python-client',
